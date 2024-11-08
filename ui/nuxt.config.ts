d9fb272907362a94a84a533159221ad837673913
sudoHub <bran@isbrandon.com>
2024-11-08 09:52:12 +0000

    shadcn nuxt added and most important shadcn components added.

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+    /**
+     *nuxt.config.ts
+     */
+    /**
+     * Import CSS or not.
+     * @default true
+     */
+    importCSS: true,
+  },
+  ui: {
+    icons: ["mdi"],
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
+  }
 })
